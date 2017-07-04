9374a42cb61cab4213d903558149bafbe7baba96
Nikita <dev.potapy4@hotmail.com>
2017-07-04 20:02:47 +0300

    Inlude files

--- WebForecastMVC/Controllers/HomeController.cs
+++ WebForecastMVC/Controllers/HomeController.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebForecastMVC.Models.Weather;
+using WebForecastMVC.Services;
 
 namespace WebForecastMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string[] cities = new string[] { "Kiev", "Lviv", "Kharkiv", "Dnipropetrovsk", "Odessa" };
+
+        // GET: Home/Index
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Cities = cities;
             return View();
-        }
+        }       
 
-        public ActionResult About()
+        // GET: Home/GetForecast
+        [HttpGet]
+        public ActionResult GetForecast (string city, int days = 7)
         {
-            ViewBag.Message = "Your application description page.";
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            Weather wr = new ForecastProvider(city).GetForecast();
+            ViewBag.Cities = cities;
+            ViewBag.Days = days;
+
+            return View("Index", wr);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
