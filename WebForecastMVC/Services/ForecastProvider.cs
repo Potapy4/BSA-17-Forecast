58a18af9078a1cebe1870dcfe355230106a719fe
Nikita <dev.potapy4@hotmail.com>
2017-07-04 20:02:27 +0300

    Complete main logic

---
+++ WebForecastMVC/Services/ForecastProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using WebForecastMVC.Models.Weather;
+using Newtonsoft.Json;
+
+namespace WebForecastMVC.Services
+{
+    public class ForecastProvider
+    {
+        private readonly string apiKey;
+        private readonly string apiUrl;
+
+        public ForecastProvider(string city)
+        {
+            apiKey = "4ca0f0f0f7e39d870ec2f3c87e5d7c51";
+            apiUrl = $"http://api.openweathermap.org/data/2.5/forecast/daily?q={city}&units=metric&APPID={apiKey}";
+        }
+
+        public Weather GetForecast()
+        {
+            Weather wr;
+
+            try
+            {
+                string jsonData = Request();
+                wr = JsonConvert.DeserializeObject<Weather>(jsonData);
+            }
+            catch
+            {
+                wr = null;
+            }
+
+            return wr;
+
+        }
+
+        private string Request()
+        {
+            string response;
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    response = wc.DownloadString(apiUrl);
+                }
+            }
+            catch
+            {
+                response = null;
+            }
+
+            return response;
+        }
+    }
+}
