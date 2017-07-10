4bd24e1fde19ccf770280d9da4bcd86129f6ec6d
Nikita <dev.potapy4@hotmail.com>
2017-07-10 14:13:40 +0300

    Faforite | History controllers

---
+++ WebForecastMVC/Controllers/FavoriteCitiesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebForecastMVC.DataBase;
+using WebForecastMVC.Models.Weather;
+
+namespace WebForecastMVC.Controllers
+{
+    public class FavoriteCitiesController : Controller
+    {
+        // GET: FavoriteCitiesController/Index
+        public ActionResult Index()
+        {
+            DBModel db = new DBModel();
+            return View(db.FavoriteCities.ToList());
+        }
+
+        // GET: FavoriteCitiesController/AddToFavorite
+        public ActionResult AddToFavorite(string city)
+        {
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                return RedirectToAction("Index");
+            }
+
+            DBModel db = new DBModel();
+            City ct = db.FavoriteCities.FirstOrDefault(x => x.Name == city);
+
+            if (ct == null) // If not exists
+            {
+                db.FavoriteCities.Add(new City { Name = city });
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: FavoriteCitiesController/Edit
+        public ActionResult Edit(int? id)
+        {
+            DBModel db = new DBModel();
+            City ct = db.FavoriteCities.FirstOrDefault(x => x.Id == id);
+
+            return View(ct);
+        }
+
+        // POST: FavoriteCitiesController/Edit
+        [HttpPost]
+        public ActionResult Edit(City c)
+        {
+            DBModel db = new DBModel();
+            City ct = db.FavoriteCities.FirstOrDefault(x => x.Id == c.Id);
+
+            if (ct != null)
+            {
+                ct.Name = c.Name; // Can change only name
+                db.Entry(ct).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: FavoriteCitiesController/Remove
+        public ActionResult Remove(int? id)
+        {
+            DBModel db = new DBModel();
+            City ct = db.FavoriteCities.First(x => x.Id == id);
+
+            if (ct != null)
+            {
+                db.FavoriteCities.Remove(ct);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebForecastMVC/Models/History.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebForecastMVC.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public double Wind { get; set; }
+        public double Humidity { get; set; }
+        public string Summary { get; set; }
+    }
+}
