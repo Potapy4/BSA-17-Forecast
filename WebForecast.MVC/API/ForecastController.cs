68e31c56809ae361dc8744dcb0b3d416873ac907
Nikita <dev.potapy4@hotmail.com>
2017-07-17 13:55:53 +0300

    Forecast API Controller

---
+++ WebForecast.MVC/API/ForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web.Http;
+using WebForecast.BLL.BusinessModels.OpenWeatherMap;
+using WebForecast.BLL.Interfaces;
+
+namespace WebForecastMVC.API
+{
+    public class ForecastController : ApiController
+    {
+        private IBusinessLogic logic;
+
+        public ForecastController(IBusinessLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET api/forecast?city=Lviv&days=3
+        public Weather Get([FromUri]string city, int days = 7)
+        {
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            return logic.GetForecast(city, days);
+        }
+    }
+}
